3d9cfdd3425a228fe05b36112d6be45042990b79
PROxZIMA <pratikbpingale9075@gmail.com>
2022-01-13 18:08:00 +0530

    Add: LeetCode problems

---
+++ LeetCode/maximum-width-of-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        queue = deque(((root, 0, 0),))
+        cur_depth = left = sol = 0
+        while queue:
+            node, depth, pos = queue.popleft()
+            if node:
+                queue.append((node.left, depth + 1, pos * 2))
+                queue.append((node.right, depth + 1, pos * 2 + 1))
+                if cur_depth != depth:
+                    cur_depth = depth
+                    left = pos
+                sol = max(pos - left + 1, sol)
+        return sol
