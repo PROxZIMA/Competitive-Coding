f3302161a73859885c7994f471b6381bac4a387b
PROxZIMA <pratikbpingale9075@gmail.com>
2022-05-23 09:22:00 +0530

    Update: Hackerrank problems

--- Hackerrank/Contests/hack-the-interview-v-asia-pacific/rerouting.py
+++ Hackerrank/Contests/hack-the-interview-v-asia-pacific/rerouting.py
@@ -1,10 +1,47 @@
-def connection(connection):
-    pass
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getMinConnectionChange' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY connection as parameter.
+#
 
 
+def getMinConnectionChange(connection):
+    assert 1 <= n <= 300000
+    assert all(1 <= i <= n for i in connection)
+
+    vis = [0] * (n + 1)
+    ct = 0
+    col = 0
+    for i in range(1, n + 1):
+        if not vis[i]:
+            cur = i
+            col += 1
+            while not vis[cur]:
+                vis[cur] = col
+                cur = connection[cur - 1]
+                ct += col == vis[cur]
+    ct -= any(i == connection[i - 1] for i in range(1, n + 1))
+    return ct
 
 
-n = int(input())
-connection = list(map(int, input().split()))
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
-print(connection(connection))
+    n = int(input().strip())
+
+    connection = list(map(int, input().rstrip().split()))
+
+    result = getMinConnectionChange(connection)
+
+    fptr.write(str(result) + "\n")
+
+    fptr.close()
