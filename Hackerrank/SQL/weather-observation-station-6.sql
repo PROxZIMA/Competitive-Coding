ea46df731cc61bd002e971e50aa6d53a704f9977
PROxZIMA <pratikbpingale9075@gmail.com>
2022-08-29 09:22:00 +0530

    Add: Hackerrank problems

---
+++ Hackerrank/SQL/weather-observation-station-6.sql
@@ -0,0 +1,30 @@
+-- SELECT DISTINCT
+-- (CITY)
+-- FROM
+--   STATION
+-- WHERE
+--   CITY LIKE "a%"
+--   OR CITY LIKE "e%"
+--   OR CITY LIKE "i%"
+--   OR CITY LIKE "o%"
+--   OR CITY LIKE "u%";
+-- SELECT DISTINCT
+-- (CITY)
+-- FROM
+--   STATION
+-- WHERE
+--   LOWER(substr(CITY, 1, 1)) IN
+--   (
+--     'a',
+--     'e',
+--     'i',
+--     'o',
+--     'u'
+--   )
+-- ;
+SELECT DISTINCT
+(CITY)
+FROM
+  STATION
+WHERE
+  CITY regexp "^[aeiou].*";
